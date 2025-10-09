3a2b88f7ee1e7996b0c61f09edc87b6bcdaab8c6
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-10-09 12:51:08 -0700

    puzzles 1 - 12

--- 333TasksInIOS/main.swift
+++ 333TasksInIOS/main.swift
@@ -1,1 +1,104 @@
-
+//// task 1
+//
+//let model:String = "MacBook Air"
+//let diagonal:Double = 13.6
+//let price:Double = 55_000
+//let color:String = "Space Gray"
+//let company:String = "Apple"
+//// это описание моего компьютера
+//
+//
+//
+//// task 2 описание внешности
+//
+//let height:Int = 173
+//var weight:Int = 90
+//let colorEye:String = "Gray"
+//let colorHair:String = "Black"
+///*
+// А это я описываю себя, как владельца данного компьютера
+// */
+//
+//
+//// task 3 Создание блокнота
+//
+//let hardcover:Bool = true
+//let checkeredSheet:Bool = true
+//var numberSheets:Int = 120
+//
+//
+//// task 4 вывести Hello, world!
+//print("Hello World!")
+//
+//
+//// task 5 abc
+//let a = 10
+//let b = 20
+//let c = a + b
+//print(c)
+//
+//// task 5 UInt
+//let d: UInt = 2
+//let e: UInt = 5
+//
+//// Сначала исправим сложение (используем a и b как требуется)
+//let sum: UInt = UInt(a) + UInt(b)
+//
+//// Для разности нужно использовать тип, который поддерживает отрицательные числа
+//let difference = Int(d) - Int(e)
+//let secondDifference = 2 - 5
+//
+//print("sum = \(sum)")
+//print("difference = \(difference)")
+//print("secondDifference = \(secondDifference)")
+//
+//
+//
+//// task 6 Число пи
+//
+//let piDouble: Double = Double.pi
+//let piFloat:Float = Float.pi
+//
+//print("piDouble -> \(piDouble)")
+//print("piFloat -> \(piFloat)")
+//
+//print(piDouble > Double(piFloat))
+//
+//
+//// task 9 Хватит ли продуктов
+//
+//let products:Bool = true
+//
+//if products {
+//    print("Можно приступить к приготовлению ужина")
+//} else {
+//    print("Нужно сходить в магазин за продуктами")
+//}
+//
+//
+//// task 10 средний балл
+//
+//var person = ("Maksim", 4.4)
+//print("person.0 ->\(person.0)")
+//print("person.1 -> \(person.1)")
+//
+//person.1 = 4.7
+//
+//print("person.0 ->\(person.0)")
+//print("person.1 -> \(person.1)")
+//
+//
+//// task 11
+//
+//let product = ("MacBook Air", 75_000, 150)
+//print(product)
+//
+//
+//
+//// task 12 typeAlias Rectangle
+//
+//typealias Rectangle = (width: Double, height: Double)
+//
+//let rectangle:Rectangle = (10, 20)
+//let area = rectangle.width * rectangle.height
+//print("area -> \(area)")
