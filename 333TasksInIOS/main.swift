17804e7f953170b25f04a936c32f4cc6186729ad
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-12-07 23:39:19 -0800

    problem solving codewars

--- 333TasksInIOS/main.swift
+++ 333TasksInIOS/main.swift
@@ -42,3 +42,119 @@ func evenOrOdd(_ number:Int) -> String {
 
 let resEvenOdd = evenOrOdd(11)
 print("Число \(resEvenOdd)")
+
+
+
+// 8 ku Запишите функцию, которая преобразует входную строку в заглавную букву.
+
+func makeUpperCase(_ s: String) -> String{
+    return  s.uppercased()
+}
+
+let resUpper = makeUpperCase("maksin")
+print(resUpper)
+
+
+// 8 ku Очень просто: для числа найдётся его противоположность (аддитивная обратная).
+
+func opposite(number: Double) -> Double {
+    return -number
+}
+
+let resOpposite = opposite(number: 10)
+print("Обратное число \(resOpposite)")
+
+
+// 8 ku Эта ката заключается в умножении данного числа на восемь, если оно чётное, и на девять в противном случае.
+
+func simpleMultiplication(_ num: Int) -> Int {
+    num % 2 == 0 ? num * 8 : num * 9
+}
+
+let resSimpleMultiplication = simpleMultiplication(11)
+print("Результат умножения \(resSimpleMultiplication)")
+
+
+// 8 ku Натан любит велосипед.
+//Поскольку Натан знает, что важно пить водный баланс, он пьёт 0,5 литра воды в час цикла.
+//Вам указывают время в часах, и вам нужно вернуть количество литров, которые Натан выпьет, округленное вниз.
+
+func litres(_ time: Double) -> Int {
+    var res = time * 0.5
+    res = res.rounded(.down)
+    return Int(res)
+}
+
+let litresTime = litres(12.3)
+print(litresTime)
+
+
+// 8 ku В этой игре герой перемещается слева направо. Игрок бросает кубик и дважды перемещает количество клеток, указанных кубиком.
+//Создайте функцию для терминальной игры, которая берёт текущую позицию героя и бросок (1-6) и возвращает новую позицию.
+
+func move(_ p: Int, _ r: Int) -> Int {
+    // your code here
+    let res = p + (r + r)
+    return res
+}
+
+let resMove = move(2, 5)
+print(resMove)     // 15
+
+
+// 8 ku When provided with a number between 0-9, return it in words. Note that the input is guaranteed to be within the range of 0-9.
+//Input: 1
+//Output: "One".
+//If your language supports it, try using a switch statement.
+
+
+func switchItUp(_ number: Int) -> String {
+    switch number {
+    case 0:
+        return "Zero"
+    case 1:
+        return "One"
+    case 2:
+        return "Two"
+    case 3:
+        return "Three"
+    case 4:
+        return "Four"
+    case 5:
+        return "Five"
+    case 6:
+        return "Six"
+    case 7:
+        return "Seven"
+    case 8:
+        return "Eight"
+    case 9:
+        return "Nine"
+    default:
+        return ""
+    }
+}
+
+let resSwitch = switchItUp(3)
+print(resSwitch)
+
+
+// 8 ku Реализовать функцию, которая преобразует данное булево значение в его строковое представление.
+//Примечание: будут предоставлены только действительные входные данные.
+
+func booleanToString(_ b: Bool) -> String {
+  return "\(b)"
+}
+
+let resBoolToStr = booleanToString(false)
+print(resBoolToStr)
+
+
+// 8 ku Теперь нужно написать функцию, которая берет аргумент и возвращает его квадрат.
+
+func square(_ num: Int) -> Int {
+  return num * num
+}
+
+let resSquare = square(3)
+print(resSquare)
